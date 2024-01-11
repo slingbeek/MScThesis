1ba2e28f60cdcbd4057fb776eb4db9ba4bd52e60
slingbeek <simonelingbeek@gmail.com>
2024-01-11 16:57:34 +0100

    small improvements

--- interpolate.py
+++ interpolate.py
@@ -18,17 +18,6 @@ import numpy as np
 from numba import float32, float64, guvectorize
 from datetime import datetime
 
-p_ref = 100000
-cp = 1004
-Rdry = 286.9
-kappa = Rdry/cp
-dtdp_crit = -2e-4 # ignoring layers where dtheta/dp > dtdp_crit
-lvls_pt = (np.cumsum([0] + [10/6] * 39 + [10/4] * 6 + [10/2] * 3 + [10] 
-    + [50] * 2 + [100] * 3) + 295).astype('float64') # new theta levels
-
-# files = sorted(glob.glob("../Data/LambertGrid/629x989/fc2017090512+???.nc"))
-# saveloc = "../Data/LambertGrid/629x989interped/"
-
 
 file, model, timing = input("Enter file name, model (cesm1/cesm2) and time interval (monthly/daily): ").split(",")
 if model == "cesm1":
@@ -51,6 +40,7 @@ else:
     print("Model not correct, must be cesm1 or cesm2")
     sys.exit()
 
+PS_input = input("Enter surface pressure file name: ")
 
 @guvectorize(
     "(float64[:], float64[:], float64[:], float32[:])",
@@ -72,118 +62,53 @@ def interp1d_gu(f, x, xi, out):
         out[i] = np.nan
         i = i + 1
 
-@guvectorize(
-    "(float64[:], float64[:], float64[:], float32[:])",
-    " (n), (n), (m) -> (m)",
-    nopython=True,
-)
-def interp1d_pres_gu(p, x, xi, out):
-    """Interpolate field p(x) to xi in ln(x) coordinates."""
-    i, imax, x0, p0 = 0, len(xi), x[0], p[0]
-    while xi[i]<x0 and i < imax:
-        out[i] = np.nan      
-        i = i + 1 
-    for x1,p1 in zip(x[1:], p[1:]):
-        while xi[i] <= x1 and i < imax:
-            gamma = np.log(p1/p0)/np.log(x1/x0)
-            pi = p1 * (xi[i]/x1)**gamma
-            out[i] = pi
-            i = i + 1
-        x0, p0 = x1, p1
-    while i < imax:
-        out[i] = np.nan
-        i = i + 1
-        
-@guvectorize(
-    "(float64[:], float64[:], float64[:])",
-    " (n), (n) -> (n)",
-    nopython=True,
-)
-def calc_dtdp_gu(t, p, dtdp):
-    """Calculate dthetadp on hybrid levels."""
-    i, imax, ln = 1, len(t), np.log
-    dtdp[0] = (ln(t[2]/t[0])/ln(p[2]/p[0]) - ln(t[2]/t[1])/ln(p[2]/p[1]) 
-               + ln(t[1]/t[0])/ln(p[1]/p[0])) * t[0]/p[0]
-    if dtdp[0] > dtdp_crit: # failure of parabola fitting
-        dtdp[0] = (t[1]-t[0])/(p[1]-p[0])
-    while not np.isnan(t[i+1]) and i < (imax - 1):
-        dtdp[i] =  t[i]/p[i] * (
-            ln(p[i]/p[i-1])*ln(t[i+1]/t[i])/(ln(p[i+1]/p[i])*ln(p[i+1]/p[i-1]))
-            + ln(p[i+1]/p[i])*ln(t[i]/t[i-1])/(ln(p[i]/p[i-1])*ln(p[i+1]/p[i-1]))
-        )
-        i = i + 1
-    dtdp[i] = (ln(t[i]/t[i-2])/ln(p[i]/p[i-2]) + ln(t[i]/t[i-1])/ln(p[i]/p[i-1]) 
-               - ln(t[i-1]/t[i-2])/ln(p[i-1]/p[i-2])) * t[i]/p[i]
-    i = i + 1
-    while i < imax:
-        dtdp[i] = np.nan
-        i = i + 1
+### Surface pressure
+PS_ds = xr.open_dataset(PS_input)
+PS = PS_ds.PS
 
-def argsort3d(da, dim): # niet nodig
-    m, n, k = da.shape
-    ids = np.ogrid[:m,:n,:k]
-    ax = da.dims.index(dim)
-    ids[ax] = da.argsort(ax)
-    return tuple(ids)
-
-def stabilize(ds, theta, pres):# niet nodig
-    """Mask theta values where dtheta/dp > dtdp_crit"""
-    assert theta.dims == ('hybrid','y','x')
-    assert pres.dims == ('hybrid','y','x')
-    vars3d = [var for var in ds.data_vars if 'hybrid' in ds[var].dims and ds[var].ndim == 3]
-    y,x = np.mgrid[:theta.shape[1],:theta.shape[2]]
-    i0 = np.zeros(theta.shape[1:], dtype=int) 
-    for i1 in range(1,len(theta)):
-        t0, p0 = theta.data[i0,y,x], pres.data[i0,y,x]
-        t1, p1 = theta.data[i1,:,:], pres.data[i1,:,:]
-        dtdp = ((t1-t0)/(p1-p0))
-        isInvalid = dtdp > dtdp_crit
-        theta.data[i1][isInvalid] = np.nan
-        for var3d in vars3d:
-            ds[var3d].data[i1][isInvalid] = np.nan
-        i0[~isInvalid] = i1
-    return ds, theta
-
-
+### Open dataset
 ds = xr.open_dataset(file)
-pres = (ds.a + ds.b * ds.p0m)
-theta = (ds.t * (p_ref/pres)**kappa)
-ds, theta = stabilize(ds, theta, pres)
-ids = argsort3d(theta, 'hybrid')
-theta[:] = theta.data[ids]
-pres[:] = pres.data[ids]
-dtdp = xr.apply_ufunc(
-    calc_dtdp_gu, theta, pres,
-    input_core_dims=[['hybrid'], ['hybrid']],
-    output_core_dims=[['hybrid']],
-    output_dtypes=[theta.dtype],
-).assign_attrs({'units':'K/Pa','long_name':'dtheta/dp'})
-ds['dtdp'] = dtdp#.transpose('hybrid','y','x')
-ds = ds.assign_coords({'theta':lvls_pt})
-ds['theta'] = ds.theta.assign_attrs(
-    {"long_name":"potential temperature","units":"K"})
-vars3d = [ds[var] for var in ds.data_vars if ds[var].ndim==3]
+pres = (ds.hyai*ds.P0 + ds.hybi * PS)
+
+### New pressure levels
+lvls_pt = np.asarray([3.5, 7.5, 14., 25., 36., 43., 52., 62., 74., 88., 103., 122., 143., 168., 198., 233., 274., 322.,
+                      379., 446., 525., 610., 691., 763., 821., 860., 887., 913., 936., 957., 976., 993.])
+
+ds = ds.assign_coords({'plev':lvls_pt})
+ds['plev'] = ds.theta.assign_attrs(
+        {"long_name":"Pressure level","units":"Pa"})
+
+### Interpolation for lev or ilev
+vars3d = [ds[var] for var in ds.data_vars if ds[var].ndim == 3]
 for var3d in vars3d:
-    var3d = var3d.astype('float64').transpose('hybrid','y','x')
-    if var3d.name != 'dtdp':
-        var3d[:] = var3d.data[ids]
-    ds[var3d.name] = xr.apply_ufunc(
-        interp1d_gu,  var3d, theta, ds.theta,
-        input_core_dims=[['hybrid'], ['hybrid'], ['theta']],
-        output_core_dims=[['theta']],
-        exclude_dims=set(('hybrid',)),  #lev
-        output_dtypes=['float32'],
-    ).transpose('theta','y','x').assign_attrs(var3d.attrs)
-ds['pres'] = xr.apply_ufunc(
-    interp1d_pres_gu,  pres, theta, ds.theta,
-    input_core_dims=[['hybrid'], ['hybrid'], ['theta']],
-    output_core_dims=[['theta']],
-    exclude_dims=set(('hybrid',)),
-    output_dtypes=['float32'],
-).transpose('theta','y','x').assign_attrs({'units':'Pa','long_name':'Pressure'})
-del ds['a']
-del ds['b']
-del ds['hybrid']
+    if 'lev' in var3d.dims:
+        var3d = var3d.astype('float64').transpose('lev','y','x')
+
+        ds[var3d.name] = xr.apply_ufunc(
+            interp1d_gu,  var3d, pres, ds.pres,
+            input_core_dims=[['lev'], ['lev'], ['plev']],
+            output_core_dims=[['plev']],
+            exclude_dims=set(('lev',)),
+            output_dtypes=['float32'],
+        ).transpose('plev','y','x').assign_attrs(var3d.attrs)
+
+        del ds['lev']
+    else:
+        var3d = var3d.astype('float64').transpose('ilev', 'y', 'x')
+
+        ds[var3d.name] = xr.apply_ufunc(
+            interp1d_gu, var3d, pres, ds.pres,
+            input_core_dims=[['ilev'], ['ilev'], ['iplev']],
+            output_core_dims=[['iplev']],
+            exclude_dims=set(('ilev',)),
+            output_dtypes=['float32'],
+        ).transpose('iplev', 'y', 'x').assign_attrs(var3d.attrs)
+
+        del ds['ilev']
+
+del ds['hyai']
+del ds['hybi']
+
 savename = saveloc + os.path.basename(file)
 print(f"Saving {savename}")
 ds.to_netcdf(savename)
