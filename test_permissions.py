0325b155f10d3f4b4b308632608f3ba1942aadb5
slingbeek <simonelingbeek@gmail.com>
2024-01-16 17:43:57 +0100

    test script for permissions poging 2

--- test_permissions.py
+++ test_permissions.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-saveloc = '/home/slingbeek/cesm2data/monthly/'
+saveloc = '/home/slingbeek/cesm_2data/monthly/'
 
-with open(saveloc+"/tijdelijk_bestand.txt", "a") as f:
+with open(saveloc+"tijdelijk_bestand.txt", "a") as f:
     f.write("Beter werkt dit")
