d789121d27ac926d6b49f8bfa78ad2c9ceb26163
slingbeek <simonelingbeek@gmail.com>
2024-02-08 14:37:59 +0100

    interpolate script updates + GLENS EM3 update

--- interpolate.py
+++ interpolate.py
@@ -80,24 +80,18 @@ ds = xr.open_dataset(file)
 
 ### New pressure levels
 if model == 'glens2':
-    lvls_pt = np.asarray([5.960300e-06, 9.826900e-06, 1.620185e-05, 2.671225e-05, 4.404100e-05, 7.261275e-05,
-                          1.197190e-04, 1.973800e-04, 3.254225e-04, 5.365325e-04, 8.846025e-04, 1.458457e-03,
-                          2.404575e-03, 3.978250e-03, 6.556826e-03, 1.081383e-02, 1.789800e-02, 2.955775e-02,
-                          4.873075e-02, 7.991075e-02, 1.282732e-01, 1.981200e-01, 2.920250e-01, 4.101675e-01,
-                          5.534700e-01, 7.304800e-01, 9.559475e-01, 1.244795e+00, 1.612850e+00, 2.079325e+00,
-                          2.667425e+00, 3.404875e+00, 4.324575e+00, 5.465400e+00, 6.872850e+00, 8.599725e+00,
-                          1.070705e+01, 1.326475e+01, 1.635175e+01, 2.005675e+01, 2.447900e+01, 2.972800e+01,
-                          3.592325e+01, 4.319375e+01, 5.167750e+01, 6.152050e+01, 7.375096e+01, 8.782123e+01,
-                          1.033171e+02, 1.215472e+02, 1.429940e+02, 1.682251e+02, 1.979081e+02, 2.328286e+02,
-                          2.739108e+02, 3.222419e+02, 3.791009e+02, 4.459926e+02, 5.246872e+02, 6.097787e+02,
-                          6.913894e+02, 7.634045e+02, 8.208584e+02, 8.595348e+02, 8.870202e+02, 9.126445e+02,
-                          9.361984e+02, 9.574855e+02, 9.763254e+02, 9.925561e+02])
+    lvls_pt = np.asarray([6.0e-06, 9.8e-06, 1.6e-05, 2.7e-05, 4.4e-05, 7.3e-05, 1.2e-04, 2.0e-04, 3.3e-04, 5.4e-04,
+                          8.8e-04, 1.5e-03, 2.4e-03, 4.0e-03, 6.6e-03, 1.1e-02, 1.8e-02, 3.0e-02, 4.9e-02, 8.0e-02,
+                          1.3e-01, 2.0e-01, 2.9e-01, 4.1e-01, 5.5e-01, 7.3e-01, 9.6e-01, 1.2e+00, 1.6e+00, 2.1e+00,
+                          2.7e+00, 3.4e+00, 4.3e+00, 5.5e+00, 6.9e+00, 8.6e+00, 1.1e+01, 1.3e+01, 1.6e+01, 2.0e+01,
+                          2.4e+01, 3.0e+01, 3.6e+01, 4.3e+01, 5.2e+01, 6.2e+01, 7.4e+01, 8.8e+01, 1.0e+02, 1.2e+02,
+                          1.4e+02, 1.7e+02, 2.0e+02, 2.3e+02, 2.7e+02, 3.2e+02, 3.8e+02, 4.5e+02, 5.2e+02, 6.1e+02,
+                          6.9e+02, 7.6e+02, 8.2e+02, 8.6e+02, 8.9e+02, 9.1e+02, 9.4e+02, 9.6e+02, 9.8e+02, 9.9e+02])
 
 else:
-    lvls_pt = np.asarray([3.5, 5., 7.5, 10., 15., 23., 33., 43., 53., 63.,
-                          74., 88., 103., 122., 143., 168., 200., 233., 274., 322.,
-                          380., 440., 500., 590., 681., 763., 821., 850., 887., 913.,
-                          936., 957., 976., 993.])
+    lvls_pt = np.asarray([3.5, 5., 7.5, 10., 15., 23., 33., 43., 53., 63., 74., 88., 103., 122., 143., 168., 200.,
+                          233., 274., 322., 380., 440., 500., 590., 681., 763., 821., 850., 887., 913., 936., 957.,
+                          976., 993.])
 
 lvls_pt = lvls_pt*100
 
@@ -110,31 +104,6 @@ ds['plev'] = ds.plev.assign_attrs(
 ### Interpolation for lev or ilev
 print("Entering interpolation loop...")
 vars3d = [ds[var] for var in ds.data_vars if ds[var].ndim == 4]
-# for var3d in vars3d:
-#     if 'lev' in var3d.dims:
-#         print("Calculating pressure for lev...")
-#         pres = (ds.hyam * ds.P0 + ds.hybm * PS)
-#         for i in range(len(ds[var3d.name]['time'])):
-#             print("Timestep ", i)
-#             ds[var3d.name][i] = xr.apply_ufunc(
-#                 interp1d_gu,  var3d.sel(time=ds[var3d.name]['time'][i]), pres.sel(time=ds[var3d.name]['time'][i]), ds.plev,
-#                 input_core_dims=[['lev'], ['lev'], ['plev']],
-#                 output_core_dims=[['plev']],
-#                 exclude_dims=set(('lev',)),
-#                 output_dtypes=['float32'],
-#             ).assign_attrs(var3d.sel(time=ds[var3d.name]['time'][i]).attrs)
-#     else:
-#         print("Calculating pressure for ilev...")
-#         ipres = (ds.hyai * ds.P0 + ds.hybi * PS)
-#         for i in range(len(ds[var3d.name]['time'])):
-#             print("Timestep ", i)
-#             ds[var3d.name][i] = xr.apply_ufunc(
-#                 interp1d_gu, var3d.sel(time=ds[var3d.name]['time'][i]), ipres.sel(time=ds[var3d.name]['time'][i]), ds.plev,
-#                 input_core_dims=[['ilev'], ['ilev'], ['plev']],
-#                 output_core_dims=[['plev']],
-#                 exclude_dims=set(('ilev',)),
-#                 output_dtypes=['float32'],
-#             ).assign_attrs(var3d.sel(time=ds[var3d.name]['time'][i]).attrs)
 
 for var3d in vars3d:
     if 'lev' in var3d.dims:
