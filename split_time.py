c6a2e2dd659a9bab984def6986260f8425109bdc
slingbeek <simonelingbeek@gmail.com>
2024-02-13 17:44:32 +0100

    ziek goeie ingeving

--- split_time.py
+++ split_time.py
@@ -12,8 +12,8 @@ from datetime import datetime
 file, model = input("Enter file name to split and model: ").split(",")
 if model == "glens2":
     saveloc = "~/GLENS2/monthly"
-    slices = [slice('2020', '2029'), slice('2030', '2039'), slice('2080', '2089'), slice('2090', '2100')]
-    names = ["202001-202912", "203001-203912", "208001-208912", "209001-210012"]
+    slices = [slice('2080', '2089'), slice('2090', '2100')]
+    names = ["208001-208912", "209001-210012"]
 
 elif model == "cmip6":
     saveloc = "~/CMIP6/monthly"
